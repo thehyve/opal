a7d011ac5436eff0c8263736d6364ad26b5d4a91
Carlos Silva <carlos@thehyve.nl>
2014-08-26 11:31:34 +0200

    Adding opal-python-client to puppet manifest/vagrant provision

--- opal-vagrant/src/main/vagrant/manifests/redhat.pp
+++ opal-vagrant/src/main/vagrant/manifests/redhat.pp
@@ -16,6 +16,21 @@ yumrepo { "opal":
   ensure    => latest,
 }
 
+yumrepo { "thehyve":
+#baseurl   => "https://repo.thehyve.nl/content/groups/public",
+  baseurl   => "https://repo.thehyve.nl/content/repositories/releases",
+  descr     => "Hyve Releases Repository",
+  enabled   => 1,
+  gpgcheck  => 0
+} -> package { 'epel-release':
+  ensure    => present,
+  provider  => rpm,
+  source    => 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm',
+} -> package { 'opal-python-client':
+  ensure    => latest,
+}
+
+
 firewall { "900 accept opal ports":
   proto     => "tcp",
   port      => [8080, 8443],
